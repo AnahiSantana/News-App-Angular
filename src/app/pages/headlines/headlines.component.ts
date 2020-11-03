5c82d0bf87328cb8014c870100ad1337e073fd14
unknown <is715773@iteso.mx>
2020-11-03 01:32:43 -0600

    headlines component added

---
+++ src/app/pages/headlines/headlines.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { NewsService } from '../../globals/services/news.service';
+
+@Component({
+  selector: 'app-headlines',
+  templateUrl: './headlines.component.html',
+  styleUrls: ['./headlines.component.scss']
+})
+export class HeadlinesComponent implements OnInit, OnChanges {
+
+  news:any[] = [];
+ 
+  constructor(private newsService:NewsService) { 
+    this.newsService = newsService;
+  }
+
+  ngOnInit(): void {
+    this.newsService.getHeadlines()
+    .then(data => {
+      this.news = data;
+      console.log(data);
+    }).catch(err => {
+      console.log(err);
+    });
+
+  }
+
+  ngOnChanges() {}
+
+}
