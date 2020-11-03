7ae075c64ddc759691a9a9a81ec69f8beb05a2de
unknown <is715773@iteso.mx>
2020-11-03 11:53:29 -0600

    NavBar changed to header componed in layouts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { NewsComponent } from './pages/news/news.component';
 import { HeadlinesComponent } from './pages/headlines/headlines.component';
 import { Page404Component } from './pages/page404/page404.component';
+import { HeaderComponent } from './layouts/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewsComponent,
     HeadlinesComponent,
-    Page404Component
+    Page404Component,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
