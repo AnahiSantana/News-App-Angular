8ba6cb7230299a2973f7788be6eafa1c877b60d2
unknown <is715773@iteso.mx>
2020-11-03 12:30:02 -0600

    Search and Source added to news component

--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -1,37 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { NewsService } from '../../globals/services/news.service';
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss']
 })
-export class NewsComponent implements OnInit {
+
+
+export class NewsComponent implements OnInit, OnChanges {
 
   title:string = "News component";
 
-  news = [
-    {
-      url: '#',
-      title: 'Noticia 1'
-    },
-    {
-      url: '#',
-      title: 'Noticia 2'
-    },
-    {
-      url: '#',
-      title: 'Noticia 3'
-    },
-    {
-      url: '#',
-      title: 'Noticia 4'
-    },
+  news:any[] = [];
+  sources:any[] = [];
 
-  ]
+  busqueda:string = "";
+  fuente:string = "";
 
-  constructor() { }
+ 
+  constructor(private newsService:NewsService) { 
+    this.newsService = newsService;
+  }
 
   ngOnInit(): void {
+    this.newsService.getSources()
+    .then(data => {
+      this.sources = data;
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
+  ngOnChanges() {}
+
+  doSearch() {
+    this.newsService.getEverything(this.busqueda,this.fuente)
+    .then(data => {
+      this.news = data;
+    }).catch(err => {
+      console.log(err);
+    });
   }
 
 }
