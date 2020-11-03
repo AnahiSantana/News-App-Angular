b9fb9b46338367fe0c998890a62e13a33e1ffeaa
unknown <is715773@iteso.mx>
2020-11-02 23:20:58 -0600

    NewsService brings news form API in getHeadlines, getEverything and getSources

--- src/app/globals/services/news.service.ts
+++ src/app/globals/services/news.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getHeadlines(pais?):Promise<any> {
+    if(!pais){
+      return this.http.get('http://localhost:3000/api/topHeadlines').toPromise();
+    }
+    return this.http.get('http://localhost:3000/api/topHeadlines/'+pais).toPromise();
+  }
+
+  getEverything(q,source?):Promise<any> {
+    if(!source){
+      return this.http.get('http://localhost:3000/api/everything?q='+ q).toPromise();
+    }
+      return this.http.get('http://localhost:3000/api/everything?q='+ q + '&source=' + source).toPromise();
+  }
+
+  getSources():Promise<any> {
+    return this.http.get('http://localhost:3000/api/sources').toPromise();
+  }
+
 }
